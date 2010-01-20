26b8d68f7146c8dcf36a3fd91fa19a6a7668c675
Edward Rudd <urkle@outoforder.cc>
2010-01-20 09:11:33 -0500

    PHP 5.2 doesn't have __DIR__. (so much for relying on Netbeans to tell me what's supported in 5.2)

--- classes/Init.php
+++ classes/Init.php
@@ -1,11 +1,12 @@
 <?php
+define("CLASS_ROOT",dirname(__FILE__).DIRECTORY_SEPARATOR);
 /**
  * Initialize class auto lloader
  */
 function AutoLoad($class) {
     $path = strtr($class,'_',DIRECTORY_SEPARATOR).'.php';
-    if (file_exists(__DIR__.$path)) {
-        require_once __DIR__.$path;
+    if (file_exists(CLASS_ROOT.$path)) {
+        require_once CLASS_ROOT.$path;
     } else {
         require_once $path;
     }
